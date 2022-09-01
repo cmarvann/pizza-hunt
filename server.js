433db81a25bce686af2fc9145ee168c44ec0d1d7
cmarvann <covingtonmarvann@gmail.com>
2022-08-31 21:32:16 -0400

    set up user model

--- server.js
+++ server.js
@@ -16,13 +16,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pizza-hun
   useUnifiedTopology: true
 });
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pizza-hunt',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+
 
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
